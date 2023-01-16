da885312984ad399e312a4f7fb4a153a19e2046a
Laura Crawford <lhcrawford97@gmail.com>
2023-01-16 20:50:25 +0000

    initialiseSea function upgraded for background image change

--- src/controller.js
+++ src/controller.js
@@ -2,5 +2,16 @@ function Controller() {
   this.initialiseSea();
 }
 
-Controller.prototype.initialiseSea = function initialiseSea() {};
+Controller.prototype.initialiseSea = function initialiseSea() {
+  const backgrounds = ["./images/water0.png", "./images/water1.png"];
+  let backgroundIndex = 0;
+  window.setInterval(() => {
+    document.querySelector(
+      "#viewport"
+    ).getElementsByClassName.backgroundImage = `url('${
+      backgrounds[backgroundIndex % backgrounds.length]
+    }')`;
+    backgroundIndex += 1;
+  }, 1000);
+};
 //this refers to the current instance of an object. In this case, this.initialiseSea() won't exist on the object itself, but it will exist on it's prototype (which JavaScript falls back to)
