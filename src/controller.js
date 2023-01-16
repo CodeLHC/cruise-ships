4b6750697f4d97ac11e83c12da227b4e46bb4c59
Laura Crawford <lhcrawford97@gmail.com>
2023-01-16 20:37:40 +0000

    ctroller constructor and script tag to include in html

--- src/controller.js
+++ src/controller.js
@@ -3,3 +3,4 @@ function Controller() {
 }
 
 Controller.prototype.initialiseSea = function initialiseSea() {};
+//this refers to the current instance of an object. In this case, this.initialiseSea() won't exist on the object itself, but it will exist on it's prototype (which JavaScript falls back to)
