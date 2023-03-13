a70466e2ff556e5ef5f3ba2a1d2cca6008cc216f
CodeLHC <lhcrawford97@gmail.com>
2023-03-13 18:10:57 +0000

    initialiseSea function updated to background image flicker

--- src/controller.js
+++ src/controller.js
@@ -3,14 +3,13 @@ function Controller() {
 }
 
 Controller.prototype.initialiseSea = function initialiseSea() {
-  const backgrounds = ["./images/water0.png", "./images/water1.png"];
+  const backgrounds = [
+    './images/water0.png',
+    './images/water1.png',
+  ];
   let backgroundIndex = 0;
   window.setInterval(() => {
-    document.querySelector(
-      "#viewport"
-    ).getElementsByClassName.backgroundImage = `url('${
-      backgrounds[backgroundIndex % backgrounds.length]
-    }')`;
+    document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
     backgroundIndex += 1;
   }, 1000);
 };
